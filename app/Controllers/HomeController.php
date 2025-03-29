612c429f82af96f70c9a780e7a20c653d4242c9a
bima-taruna <bimataruna21@gmail.com>
2025-03-29 17:30:25 +0700

    add upload method for file handling

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Controllers;
 
@@ -11,5 +11,13 @@ class HomeController
     public function index(): View
     {
         return View::make('index');
+    }
+
+    public function upload()
+    {
+        $filePath = STORAGE_PATH . '/' . $_FILES['transaction']['name'];
+        move_uploaded_file($_FILES['transaction']['tmp_name'], $filePath);
+
+        var_dump(pathinfo($filePath));
     }
 }
